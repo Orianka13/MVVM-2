3cc1cca2b0d962407aa823ed9dec0bc21e65fef2
Olesia <odobrogor@gmail.com>
2021-06-21 18:34:49 +0300

    UITableView with MVC

---
+++ MVVM-2/Model/Profile.swift
@@ -0,0 +1,14 @@
+//
+//  Profile.swift
+//  MVVM-2
+//
+//  Created by Олеся Егорова on 21.06.2021.
+//
+
+import Foundation
+
+struct Profile {
+    var name: String
+    var secondName: String
+    var age: Int
+}
---
+++ MVVM-2/View/TableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  TableViewCell.swift
+//  MVVM-2
+//
+//  Created by Олеся Егорова on 21.06.2021.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var fullNameLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    
+
+}
