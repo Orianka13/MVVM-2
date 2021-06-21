34b0784948f8e7d3b9cd3d374d485e87494a5960
Olesia <odobrogor@gmail.com>
2021-06-21 17:58:51 +0300

    Initial Commit

---
+++ MVVM-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM-2
+//
+//  Created by Олеся Егорова on 21.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
