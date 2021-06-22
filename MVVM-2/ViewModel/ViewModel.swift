5f9bb7be5f78fcfe0e471e501e01f7bba229208a
Olesia <odobrogor@gmail.com>
2021-06-22 18:26:08 +0300

    pass data to DetailVC

--- MVVM-2/ViewModel/ViewModel.swift
+++ MVVM-2/ViewModel/ViewModel.swift
@@ -8,7 +8,9 @@
 import Foundation
 
 class ViewModel: TableViewViewModelType {
- 
+    
+    private var selectedIndexPath: IndexPath?
+
     var profiles = [Profile(name: "John", secondName: "Smith", age: 33),
                     Profile(name: "Piter", secondName: "Parker", age: 21),
                     Profile(name: "Capitan", secondName: "America", age: 35)]
@@ -20,5 +22,14 @@ class ViewModel: TableViewViewModelType {
     func cellViewModel(forIndexPath indexPath: IndexPath) -> TableViewCellViewModelType? {
         let profile = profiles[indexPath.row]
         return TableViewCellViewModel(profile: profile)
+    }
+    
+    func viewModelForSelectedRow() -> DetailViewModelType? {
+        guard let selectedIndexPath = selectedIndexPath else { return nil }
+        return DetailViewModel(profile: profiles[selectedIndexPath.row])
+    }
+    
+    func selectRow(atIndexPath indexPath: IndexPath) {
+        self.selectedIndexPath = indexPath
     }
 }
